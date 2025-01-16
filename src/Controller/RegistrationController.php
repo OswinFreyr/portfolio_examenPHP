0a5994aff99918e2a38ce6e85700dc731b397d6a
TanguyBoonty <pisanotanguy@gmail.com>
2025-01-16 22:45:50 +0100

    p5

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,7 +27,7 @@ class RegistrationController extends AbstractController
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
             $user->setRoles(['ROLE_USER']);
-            // $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+            // $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']); // rajout du role admin sur un compte
 
             $entityManager->persist($user);
             $entityManager->flush();
